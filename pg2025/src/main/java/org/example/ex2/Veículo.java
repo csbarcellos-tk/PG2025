5b5efa965784d6053e4aa0f6e02a388a82d2ef56
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-08-19 10:32:29 -0300

    Primeiro commit do projeto PG2025

---
+++ pg2025/src/main/java/org/example/ex2/Veículo.java
@@ -0,0 +1,12 @@
+package org.example.ex2;
+
+public class Veículo {
+
+    private String Placa;
+    private String cor;
+    private int passageiros;
+    private int Velocidade;
+    private int Consumo;
+
+    }
+}
